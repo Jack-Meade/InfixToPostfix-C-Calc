1ac37facd51bbd0fa69ec552fa87b59772a1a643
Jack-Meade <jacmeade@hotmail.com>
2019-10-23 22:06:35 +0100

    Working on converting to token

--- tokenizer.c
+++ tokenizer.c
@@ -1,15 +1,17 @@
-// CS3500 Tokenizer
+// CS3500 Project - Tokenizer
 // Conor Patrick Mc Donald, Daniels Leonards Bindemans, Jack Meade
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include "token.c"
 
-#define BSIZE 201
+#define BFSIZE 201
+#define BTSIZE 10
 
 char *parse_file(char *filename) {
-    char *buffer = malloc(BSIZE);
+    char *bufferf = malloc(BFSIZE);
     FILE *fp = fopen("input.txt", "r");
     char c = 0;
     int bp = 0;
@@ -18,23 +20,37 @@ char *parse_file(char *filename) {
         switch (c) {
             case '\n':
             case ' ':
-            case -1:
                 break;
             default:
-                buffer[bp] = c;
+                bufferf[bp] = c;
                 bp ++;
         }
-
     }
 
-    return buffer;
+    return bufferf;
 }
 
 int main(int argc, char**argv) {
     char *pstring = parse_file("input.text");
     printf("%s\n", pstring);
 
-    /* convert to tokens */
+    char *buffert = malloc(BTSIZE);
+    int bp = 0;
+    int pp = 0;
+    int isfloat = 0;
+
+    while (pstring[pp] != EOF) {
+        if (isdigit(pstring[pp]) || pstring[pp] == '.') {
+            buffert[bp] = pstring[pp];
+            pp ++; bp ++;
+
+            if (pstring[pp] == '.') { isfloat = 1; }
+
+            continue;
+        } else {
+            // TODO: when pstring[pp] is operator
+        }
+    }
 
 	return 0;
 }
