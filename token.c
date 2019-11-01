635c044a151c92f738f641a286e979cc4bdd1981
Jack-Meade <jacmeade@hotmail.com>
2019-11-01 21:04:35 +0000

    Added documentation

--- token.c
+++ token.c
@@ -1,18 +1,23 @@
+// CS3500 Project - Tokenizer
+// Conor Patrick Mc Donald, Daniels Leonards Bindemans, Jack Meade
+
 #include <string.h>
 
 typedef struct __token {
-    char type;
-    char *value;
+    char type;          // Type: int, float, operator
+    char *value;        // Value: of type
 } token;
 
-void printToken(token *t) {
+// Prints Token contents
+void print_token(token *t) {
     printf("Token type=%c, value=%s\n", t->type, t->value);
 }
 
+// Creates and returns Token struct
 token *create_token(char type, char *value) {
-    token *t = (token *)malloc(sizeof(token));
-    t->type = type;
-    t->value = malloc(strlen(value)+1);
-    strcpy(t->value, value);
+    token *t = (token *)malloc(sizeof(token));  // Token size is variable
+    t->type = type;                             // Type indicated by single char
+    t->value = malloc(strlen(value)+1);         // Value size is length on string+1 for NULL char
+    strcpy(t->value, value);                    // Have to copy otherwise Token value is tied to original string
     return t;
 }
