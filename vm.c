7b2267d751f175cda982caedfb11303993c03703
Jack-Meade <jacmeade@hotmail.com>
2019-11-05 21:16:40 +0000

    Added documentation, minor improvements to code

--- vm.c
+++ vm.c
@@ -6,34 +6,31 @@
 #include <string.h>
 #include <math.h>
 
+// Executes VM using code generated
 float execute(char *filename) {
     FILE *fp;
-    char *line;
-    char *instruction;
-    double operands[100];
-    ssize_t op = -1;
-    size_t len = 0;
-    ssize_t read;
+    char *instruction;                                      // Buffer which holds current instruction
+    float operands[100];                                    // Operand stack
+    int op = -1;                                            // Pointer to current operand in stack
+    size_t len = 0;                                         // Pointer to size of buffer (needed for getline())
     fp = fopen(filename, "r");
 
-    if (fp == NULL) { exit(1); }
+    if (fp == NULL) { exit(1); }                            // If file not found, exit with error
 
-    while ((read = getline(&line, &len, fp)) != EOF) {
-        if (read != 4) {
-            instruction = strsep(&line, " ");
-            instruction = strsep(&line, " ");
+    while ((getline(&instruction, &len, fp)) != EOF) {      // While not at end of code generated
+        // Determine next instruction
+        if      (strcmp(instruction, "EXP\n") == 0) { operands[op-1] = pow(operands[op-1], operands[op]); op--; }
+        else if (strcmp(instruction, "ADD\n") == 0) { operands[op-1] =     operands[op-1] + operands[op]; op--; }
+        else if (strcmp(instruction, "SUB\n") == 0) { operands[op-1] =     operands[op-1] - operands[op]; op--; }
+        else if (strcmp(instruction, "MUL\n") == 0) { operands[op-1] =     operands[op-1] * operands[op]; op--; }
+        else if (strcmp(instruction, "DIV\n") == 0) { operands[op-1] =     operands[op-1] / operands[op]; op--; }
+        else {
+            // Else we have a number and need to put into stack
             instruction[strcspn(instruction, "\n")] = 0;
-
             operands[++op] = strtof(instruction, NULL);
-        } else {
-            if      (strcmp(line, "EXP\n") == 0) { operands[op-1] = pow(operands[op-1], operands[op]); op--; }
-            else if (strcmp(line, "ADD\n") == 0) { operands[op-1] =     operands[op-1] + operands[op]; op--; }
-            else if (strcmp(line, "SUB\n") == 0) { operands[op-1] =     operands[op-1] - operands[op]; op--; }
-            else if (strcmp(line, "MUL\n") == 0) { operands[op-1] =     operands[op-1] * operands[op]; op--; }
-            else if (strcmp(line, "DIV\n") == 0) { operands[op-1] =     operands[op-1] / operands[op]; op--; }
         }
     }
-    free(line);
+    free(instruction);
     fclose(fp);
     return operands[op];
 }
