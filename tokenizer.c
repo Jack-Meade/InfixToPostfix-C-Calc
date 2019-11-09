b84d8f503dd324100b83a5b9eff329756bcf3108
Jack-Meade <jacmeade@hotmail.com>
2019-11-09 14:14:03 +0000

    Updated to handle % operator, refactored code, added documentation

--- tokenizer.c
+++ tokenizer.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <ctype.h>
 
-#define BFSIZE 101                                                  // Memory allocation for full buffer size
+#define BFSIZE 100                                                      // Memory allocation for full buffer size
 #define OUTPUT_FILE "tokens.txt"
 #define INPUT_FILE "input.txt"
 
@@ -14,18 +14,29 @@
 void write_file(char *output) {
     FILE *fp = NULL;
     if (fp = fopen(OUTPUT_FILE, "w"))   { fprintf(fp, "%s", output); fclose(fp); }
-    else                             { fprintf(stderr, "Error: Could not create tokens.txt\n"); exit(4); }
+    else                                { fprintf(stderr, "Error: Could not create tokens.txt\n"); exit(4); }
+}
+
+// Marks end of operand and places operator in output
+void end_of_token(int *new_num, char *output, int *op, char c, int* illegal_operator) {
+    if (!*new_num) { output[++*op] = '\n'; }                            // Mark end of token by starting newline
+    *new_num      = 1;                                                  // Next digit/decimal point will be a new num
+    output[++*op] = 'o';                                                // Place operator in output
+    output[++*op] = ',';
+    output[++*op] = c;
+    output[++*op] = '\n';
+    *illegal_operator = 1;
 }
 
 // Reads in file & formats
 char *parse_file(FILE *fp) {
-    char *output = (char *)malloc(sizeof(char) * BFSIZE);           // Output used in I2P
-    char c;
-    int bp = -1;                                                    // Pointer to current position on buffer
-    int op = -1;                                                    // Pointer to current position on output
-    int new_num = 1;                                                // Indicates if new num while processing buffer
-    int lastCharNotNum = 1;
-    int num_is_neg = 0;
+    char *output = (char *)malloc(sizeof(char) * BFSIZE);               // Output used in I2P
+    char c;                                                             // Current char in buffer
+    int bp = -1;                                                        // Pointer to current position in buffer
+    int op = -1;                                                        // Pointer to current position in output
+    int new_num = 1;                                                    // Indicates if new num while processing buffer
+    int illegal_operator = 1;                                           // Indicates if operator is valid as current char
+    int num_is_neg = 0;                                                 // eg. 25 + -40 is a valid infix expression
 
     // If next char isn't EOF,
     //  decide if it should go on buffer or not
@@ -36,21 +47,16 @@ char *parse_file(FILE *fp) {
                 break;
             case '(':
             case ')':
-                if (!new_num) { output[++op] = '\n'; }                  // Mark end of token by starting newline
-                new_num      = 1;                                       // Next digit/decimal point will be a new num
-                output[++op] = 'o';                                     // Place operator on buffer, with type and value
-                output[++op] = ',';
-                output[++op] = c;
-                output[++op] = '\n';
-                lastCharNotNum = 1;
-                if (c == ')') { lastCharNotNum = 0; }
+                end_of_token(&new_num, output, &op, c, &illegal_operator);
+                if (c == ')') { illegal_operator = 0; }
                 break;
             case '+':
             case '-':
             case '*':
             case '/':
             case '^':
-                if (lastCharNotNum) {
+            case '%':
+                if (illegal_operator) {
                     if (num_is_neg || c != '-') {
                         fclose(fp);
                         fprintf(stderr, "Error: Invalid infix expression\n");
@@ -59,13 +65,7 @@ char *parse_file(FILE *fp) {
                         num_is_neg = 1;
                     }
                 } else {
-                    if (!new_num) { output[++op] = '\n'; }                  // Mark end of token by starting newline
-                    new_num      = 1;                                       // Next digit/decimal point will be a new num
-                    output[++op] = 'o';                                     // Place operator on buffer, with type and value
-                    output[++op] = ',';
-                    output[++op] = c;
-                    output[++op] = '\n';
-                    lastCharNotNum = 1;
+                    end_of_token(&new_num, output, &op, c, &illegal_operator);
                     num_is_neg = 0;
 
                 }
@@ -75,16 +75,16 @@ char *parse_file(FILE *fp) {
                     new_num      = 0;                                   //   Set it so next char won't be set as a new num
                     output[++op] = 'n';                                 //   Set it's type
                     output[++op] = ',';
-                    if (num_is_neg) {
-                        output[++op] = '-';
+                    if (num_is_neg) {                                   //  If number is negative
+                        output[++op] = '-';                             //   Mark is as such
                     }
                 }
-                output[++op] = c;
-                lastCharNotNum = 0;
-                num_is_neg = 0;
+                output[++op] = c;                                       // Place char in output
+                illegal_operator = 0;                                   // An operator is legal after a num
+                num_is_neg = 0;                                         // Number
         }
     }
-    if (lastCharNotNum) {
+    if (illegal_operator) {
         fclose(fp);
         fprintf(stderr, "Error: Trailing operator/period in expression\n");
         exit(3);
@@ -96,7 +96,7 @@ char *parse_file(FILE *fp) {
 // Converts file to an array of tokens
 void convert_file_to_tokens(FILE *fp) {
     char *output = parse_file(fp);                                  // Read in file & format
-    write_file(output);                                // Write out to tokens.txt
+    write_file(output);                                             // Write out to tokens.txt
 }
 
 int main(int argc, char**argv) {
