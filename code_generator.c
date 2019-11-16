f8ff4c905e666d3f79cde3f84875c307488331ab
danielsleo <117430494@umail.ucc.ie>
2019-11-16 16:31:51 +0000

    Added documentation to code_generator

--- code_generator.c
+++ code_generator.c
@@ -6,48 +6,48 @@
 #include <string.h>
 
 char *parse_file(FILE*fp){
-    char *parsed;                   //Hold parsed characters
-    char *chars;                    //Input buffer Holds characters for getline()
-    size_t len = 0;                 //Holds the size of the input buffer,
+    char *parsed;                            //Hold parsed characters
+    char *chars;                             //Input buffer Holds characters for getline()
+    size_t len = 0;                          //Holds the size of the input buffer,
     char *str  = malloc(sizeof(char) * 400); //Buffer with limited to 400 char.
-    while (getline(&chars, &len, fp) != EOF){
-        char *s = strsep(&chars, ",");
-        if (strcmp(s, "o") == 0){
-            s = strsep(&chars, ",");
-            if      (strcmp(s, "^\n") == 0)  {parsed = "EXP\n";}
+    while (getline(&chars, &len, fp) != EOF){                       //While not at the end of the file
+        char *s = strsep(&chars, ",");                              //Split the string ',' to get first part
+        if (strcmp(s, "o") == 0){                                   //If it's 'o' then do operators
+            s = strsep(&chars, ",");                                //Split again to get second part of string
+            if      (strcmp(s, "^\n") == 0)  {parsed = "EXP\n";}    //Compare and transform operands
             else if (strcmp(s, "%\n") == 0)  {parsed = "MOD\n";}
             else if (strcmp(s, "*\n") == 0)  {parsed = "MUL\n";}
             else if (strcmp(s, "/\n") == 0)  {parsed = "DIV\n";}
             else if (strcmp(s, "+\n") == 0)  {parsed = "ADD\n";}
             else if (strcmp(s, "-\n") == 0)  {parsed = "SUB\n";}
-            else{
+            else{                                                   //else the operand isn't supported
                 fprintf(stderr, "Unsupported operand: %s \n", s);
-                exit(-1);
+                exit(-1);                                           //exit the program.
             }
-        }
+        }                                    //else if it isn't an operand then it is a number
         else{parsed = strsep(&chars, ",");}
-        strcat(str, parsed);
+        strcat(str, parsed);                 //concat the number or operand to buffer 'str'.
     }
-    strcat(str,"\0");
-    fclose(fp);
-    return str;
+    strcat(str,"\0");       //at the end we add null character.
+    fclose(fp);             //close the file
+    return str;             //return the new string.
 }
 int write_file(FILE*fp, char *c){
-    fprintf(fp, "%s", c);
+    fprintf(fp, "%s", c);       //Write the string to the new file
     return 0;
 }
 
 int main(int argc, char**argv) {
     //Error detection
     FILE *fp = NULL;
-    fp = fopen("i2p.txt","r");
-    if (fp == NULL){
-        fprintf(stderr, "File %s does not exist.\n");
+    fp = fopen("i2p.txt","r");                          //Try to open file
+    if (fp == NULL){                                    //If the file pointer is null
+        fprintf(stderr, "File %s does not exist.\n");   //Then the file doesn't exist
         return 1;
     }
     //End of error detection
-    char *str = parse_file(fp);
-    FILE *output = fopen("code.txt", "w");
-    write_file(output, str);
-    return 0;
+    char *str = parse_file(fp);                     //parse the file
+    FILE *output = fopen("code.txt", "w");          //create new file
+    write_file(output, str);                        //write to new file the parsed data
+    return 0;           
 }
