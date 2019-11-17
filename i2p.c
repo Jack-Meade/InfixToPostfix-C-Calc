b63cb52a9fb7c9724907f9107ec477f0d54b9eaa
Conor <conor@test.com>
2019-11-17 13:37:39 +0000

    Fixed Pop(), added write function.

--- i2p.c
+++ i2p.c
@@ -1,116 +1,115 @@
+// CS3500 Project - Infix To Postfix
+// Conor Patrick Mc Donald, Daniels Leonards Bindemans, Jack Meade
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
+// Prints formatted output to file
+void write_file(char *output) {
+    FILE *fp = NULL;
+    if (fp = fopen("i2p.txt", "w"))     { fprintf(fp, "%s", output); fclose(fp); }
+    else                                { fprintf(stderr, "Error: Could not create i2p.txt\n"); exit(1); }
+}
 
 /*Pop the top object off the tack and return it*/
-char pop(char *stack, int *top){
-  if(*top == -1)  // Stack is empty so return -1
-      return '?';
-  else
-    //printf("%c\n", stack[*top-1]);
-      return stack[*top--];  // Return what what the top of stack and decrement stack cursor by 1
+char pop(char *stack, int *top) {
+    char top_of_stack = stack[*top];
+    stack[*top]= '\0';
+    --*top;
+    return top_of_stack;  // Return what what the top of stack and decrement stack cursor by 1
 }
 
 void push(char *stack, int *top, char x){ // Add next char to top of stack
-  stack[++*top] = x; // Inc the stack cursor by 1
+    stack[++*top] = x; // Inc the stack cursor by 1
 }
 
 int precedence(char x){
-  if(x == '^'){  // Exponent has the highest priority
-    return 3;
-  }
+    switch(x) {
+        case '^':
+            return 3; break;
 
-  else if(x == '*' || x == '/' || x == '%'){  // Multiplication and Division/Modulo have second Highest
-      return 2;
-  }
+        case '*': case '/': case '%':
+            return 2; break;
 
-  else if(x == '+' || x == '-'){  // Addition and subtraction have third highest priority(1)
-      return 1;
-  }
-  else{  // Brackets have a priority of 0
-      return 0;
-  }
+        case '+': case '-':
+            return 1; break;
 
+        default:
+            return 0;
+    }
+}
+
+// Writes operator onto output
+void push_operator(char *output, int *op, char *stack, int *top) {
+    output[++*op] = 'o';
+    output[++*op] = ',';
+    output[++*op] = pop(stack, &*top);
+    output[++*op] = '\n';
 }
 
 char *parse_file(FILE*fp){
-    char *stack = (char *)malloc(100);  // The stack, 60 bytes
+    char *stack = (char *)malloc(100);  // The stack
     int top = -1; // initalise the top of stack
     char *chars;                    //Input buffer Holds characters for getline()
     size_t len = 0;                 //Holds the size of the input buffer,
-    char *output = (char *)malloc(100);
-    int i=-1;
-    int lp=-1;
-    char op;
-    char val;
+    char *output = (char *)malloc(400);
+    int op = -1;
+    int lp = -1;
+    char operator;
+
     while (getline(&chars, &len, fp) != EOF){
         char *s = strsep(&chars, ",");
         if (strcmp(s, "n") == 0) { // numbers are put directly into output queue
-          lp = -1;
-          output[++i] = 'n';
-          output[++i] = ',';
-          while (chars[++lp] != '\0') {
-            output[++i] = chars[lp];
-          }
-          //printf("%s\n", output);
-        }
-        else if (strcmp(s, "o") == 0){
+            lp = -1;
+            output[++op] = 'n';
+            output[++op] = ',';
 
-            s = strsep(&chars, ",");  //operands go straight to the stack
-            s[strcspn(s, "\n")] = 0;
-            printf("%s Pushed to Stack\n",s);
-            op = s[0];
-            if (op == ')') {
-                printf("%c Start While loop\n",op );
-                while((val = pop(stack, &top)) != '('){ //Pop from stack til opening bracket found
-                  //printf("%c\n",val);
-                  //push(stack, &top,'(');
-                  if((char)val == '('){
-                    printf("True\n");
-                  }
+            while (chars[++lp] != '\0') {
+                output[++op] = chars[lp];
+            }
 
-                  //printf("%d\n", val);
-                  output[++i] = 'o';
-                  output[++i] = ',';
-                  output[++i] = val;
-                  output[++i] = '\n';
+        } else if (strcmp(s, "o") == 0){ //operands go straight to the stack
+            s = strsep(&chars, ",");
+            operator = s[0];
 
+            if (operator == ')') {
+                while(stack[top] != '(') { // Pop from stack til opening bracket found
+                    push_operator(output, &op, stack, &top);
                 }
+                pop(stack, &top); // Pops ( from stack
+
+            } else if (operator == '(') {
+                push(stack, &top, operator);
+
+            } else {
+                while(precedence(stack[top]) >= precedence(operator)){
+                    push_operator(output, &op, stack, &top);
+                }
+                push(stack, &top, operator);
             }
-            else if (op == '(') {push(stack, &top, op);}
-            else {
-              while(top != -1 && precedence(stack[top]) >= precedence(op)){
-                output[++i] = 'o';
-                output[++i] = ',';
-                output[++i] = pop(stack, &top);
-                output[++i] = '\n';
-              }
-              push(stack, &top, op);
-              //printf("%s\n", "poppop");
-            }
+
+        } else { // Otherwise it is a number
+            printf("Bad input\n");
         }
-        else {
-          printf("Bad input\n");
-        }  // Otherwise it is a number
-
-
     }
-    //printf("%s\n",output );
-    return 0;
+    while (top != -1) { // Popping leftovers from stack
+        push_operator(output, &op, stack, &top);
+    }
+
+    free(stack);
+    fclose(fp);
+    return output;
   }
 
-    int main(int argc, char const *argv[]) {
-      //opening the file
-      FILE *fp = NULL;
-      fp = fopen("tokens.txt","r");
-      if (fp == NULL){
-          fprintf(stderr, "File does not exist.\n");
-          return 1;
-      }
-      // Generate postfix from file
-      parse_file(fp);
+int main(int argc, char const *argv[]) {
+    //opening the file
+    FILE *fp = NULL;
+    fp = fopen("tokens.txt","r");
+    if (fp == NULL){
+        fprintf(stderr, "File does not exist.\n");
+        return 1;
     }
-
-    //return str;
+    // Generate postfix from file
+    write_file(parse_file(fp));
+}
