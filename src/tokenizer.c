08c698e5f8b179270003571d77512d6b63f509a0
Jack-Meade <jack-meade@outlook.com>
2019-11-17 22:24:52 +0000

    Removing dedicated test version of source files

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -128,3 +128,26 @@ int main(int argc, char**argv) {
 
     return 0;
 }
+
+// Test suite
+// TESTS {
+//     #define TEST_INPUT_FILE         "tests/test_input.txt"
+//     #define TEST_INPUT_EXPRESSION   "10 +5/ 10"
+//     #define TEST_OUTPUT_EXPRESSION  "n,10\no,+\nn,5\no,/\nn,10\n"
+//
+//     ok(BUFFER_SIZE == 400,                   "Buffer size set correct");
+//     ok(INPUT_FILE  == "input.txt",           "Input file set correct");
+//     ok(OUTPUT_FILE == "output/tokens.txt",   "Output file set correct");
+//
+//     char *line;
+//     size_t len;
+//     write_file(TEST_INPUT_FILE, TEST_INPUT_EXPRESSION);
+//     FILE *fp1 = fopen(TEST_INPUT_FILE, "r");
+//     isnt_null(fp1,                  "Able to read files");
+//     getline(&line, &len, fp1);
+//     is(line, TEST_INPUT_EXPRESSION, "Able to write files");
+//
+//     FILE *fp2 = fopen(TEST_INPUT_FILE, "r");
+//     char *test_output = parse_file(fp2);
+//     is(test_output, TEST_OUTPUT_EXPRESSION, "Able to tokenize input file");
+// }
