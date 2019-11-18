751640b3ee9b4525061ee6a3078d2421302e9d9a
Jack-Meade <jack-meade@outlook.com>
2019-11-18 20:04:46 +0000

    Tidied code, added missing error mesage

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -9,13 +9,11 @@ int main(int argc, char**argv) {
     if (fp = fopen(INPUT_FILE, "r")) {
         char *output = convert_file_to_tokens(fp);                     // Read in file & format
 
-        if      (strcmp(output, ERROR_INVALID_CHAR)         == 0)       { fprintf(stderr, "%s\n", ERROR_INVALID_CHAR);          return 1; }
-        else if (strcmp(output, ERROR_INVALID_EXPRESSION)   == 0)       { fprintf(stderr, "%s\n", ERROR_INVALID_EXPRESSION);    return 1; }
-        else if (strcmp(output, ERROR_TRAILING_OPERATOR)    == 0)       { fprintf(stderr, "%s\n", ERROR_TRAILING_OPERATOR);     return 1; }
+        if      (strcmp(output, ERROR_INVALID_CHAR)       == 0) { fprintf(stderr, "%s\n", ERROR_INVALID_CHAR);       return 1; }
+        else if (strcmp(output, ERROR_INVALID_EXPRESSION) == 0) { fprintf(stderr, "%s\n", ERROR_INVALID_EXPRESSION); return 1; }
+        else if (strcmp(output, ERROR_TRAILING_OPERATOR)  == 0) { fprintf(stderr, "%s\n", ERROR_TRAILING_OPERATOR);  return 1; }
         else    { write_file(OUTPUT_FILE, output); }                   // Write tokens to output file
 
-    } else {
-        return 1;
-    }
+    } else { fprintf(stderr, "%s\n", ERROR_NO_INPUT); return 1; }
     return 0;
 }
