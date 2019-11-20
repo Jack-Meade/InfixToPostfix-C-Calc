ae2d56b499131ecc4f5c414b41605aefb21c8172
Jack-Meade <jack-meade@outlook.com>
2019-11-20 00:22:50 +0000

    Made bulk of tests

--- tests/vm.c
+++ tests/vm.c
@@ -8,8 +8,50 @@
 TESTS {
     #define TEST_INPUT_FILE "tests/input.txt"
     #define TEST_INPUT      "-25\n7.25\n3\nMUL\nSUB\n"
-    #define TEST_OUTPUT     -46.75
+    #define TEST_OUTPUT     "-46.7500"
 
-    write_file(TEST_INPUT_FILE, TEST_INPUT);
-    ok(TEST_OUTPUT == execute(TEST_INPUT_FILE), "Able to execute instructions");
+    #define TEST_NO_INPUT       "tests/in.txt"
+    #define TEST_INVALID_INST1  "-25\n7.25\n3\nMUL\nSU\n"
+    #define TEST_INVALID_INST2  "-k5\n7.25\n3\nMUL\nSUB\n"
+    #define TEST_INVALID_INST3  "-25\n7..25\n3\nMUL\nSUB\n"
+    #define TEST_INVALID_INST4  "--25\n7.25\n3\nMUL\nSUB\n"
+    #define TEST_ZERO_DIV       "-25\n0\nDIV\n"
+    #define TEST_MISSING_INST   "-25\n7.25\n3\nMUL\n"
+
+    char *test_result;
+
+    char test_inputs[6][30] = {
+        TEST_INVALID_INST1,
+        TEST_INVALID_INST2,
+        TEST_INVALID_INST3,
+        TEST_INVALID_INST4,
+        TEST_ZERO_DIV,
+        TEST_MISSING_INST
+    };
+
+    char test_outputs[6][40] = {
+        ERROR_INVALID_INST,
+        ERROR_INVALID_INST,
+        ERROR_INVALID_INST,
+        ERROR_INVALID_INST,
+        ERROR_ZERO_DIV,
+        ERROR_MISSING_INST
+    };
+
+    char test_messages[6][40] = {
+        "Incomplete instruction in input file",
+        "Invalid number in input file",
+        "Double period in input file",
+        "Double negative in input file",
+        "Zero division in input file",
+        "Missing instruction in input file"
+    };
+
+    is(execute(TEST_NO_INPUT), ERROR_NO_INPUT, "Input file not found");
+
+    for (int i = 0; i < 6; i++) {
+        write_file(TEST_INPUT_FILE, test_inputs[i]);
+        test_result = execute(TEST_INPUT_FILE);
+        is(test_result, test_outputs[i], test_messages[i]);
+    }
 }
