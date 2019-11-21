fe440408c0d332dc512491eb132a6fc4a4a2cdfb
Conor <conor@test.com>
2019-11-21 14:00:36 +0000

    Fixed tests, All 5 Pass.

--- tests/i2p.c
+++ tests/i2p.c
@@ -11,13 +11,13 @@ TESTS {
     #define TEST_EXPRESSION_OUTPUT  "n,10\no,+\nn,5\no,/\nn,10\n"
     #define TEST_OPERATOR_INPUT     '+'
     #define TEST_OPERATOR_OUTPUT    "o,+\n"
-    #define TEST_EXTRA_OPERATOR     "5++5*/7"
+    #define TEST_EXTRA_OPERATOR     "n,5\no,+\no,+\nn,5\n"
 
     //output
     #define TEST_NO_INPUT           "tests/in.txt"
     #define TEST_BAD_INPUT          "10 +5/ c10"
-    #define TEST_MISMATCHED_BRACKETS "5 +(2+4))"
-    #define TEST_MISSING_OPERATOR   "456"
+    #define TEST_MISMATCHED_BRACKETS "o,(\no,(\no,)\n"
+    #define TEST_MISSING_OPERATOR   "n,4\nn,5\nn,6\n"
 
 
     char *test_result;
@@ -31,7 +31,7 @@ TESTS {
     };
 
     // Create expected test outputs
-    char test_outputs[4][50] = {
+    char test_outputs[4][80] = {
         ERROR_BAD_INPUT,
         ERROR_MISMATCHED_BRACKETS,
         ERROR_MISSING_OPERATOR,
@@ -39,21 +39,21 @@ TESTS {
     };
 
     // Create test messages
-    char test_messages[4][50] = {
+    char test_messages[4][80] = {
         "Invalid character in input file",
-        "Missing opening or closing bracket"
-        "Missing operator after bracket"
+        "Missing opening or closing bracket",
+        "Missing operator after bracket",
         "Found 2 operators in a row"
     };
 
     // Need to test outside loop as it changes input file
-    test_result = parse_file(TEST_NO_INPUT);
-    is(test_result, ERROR_NO_INPUT, "Input file not found");
+    test_result = infix_to_postfix(TEST_NO_INPUT);
+    is(test_result, ERROR_NO_INPUT_I2P, "Input file not found");
 
     // Go through test inputs, comparing result to expected output, and display descriptive message
-    for (int i = 0; i < 2; i++) {
+    for (int i = 0; i < 4; i++) {
         write_file(TEST_INPUT_FILE, test_inputs[i]);
-        test_result = parse_file(TEST_INPUT_FILE);
+        test_result = infix_to_postfix(TEST_INPUT_FILE);
         is(test_result, test_outputs[i], test_messages[i]);
     }
 }
